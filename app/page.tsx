41d55cfb5b5569e9cd45789d0fb69ee5687a5f37
Najib <najibqazi777@gmail.com>
2024-08-06 23:51:25 +0500

    completed hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,19 @@
 import Hero from "@/components/Hero";
 import ThemeSwitch from "@/components/ThemeSwitch";
+import { FloatingNav } from "@/components/ui/FloatingNavBar";
+import { navItems } from "@/data";
 
 export default function Home() {
   return (
     <>
       <main className=" relative flex justify-center items-center  flex-screen flex-col overflow-hidden mx-auto sm:px-10 px-5">
         <div className=" max-w-7xl w-full">
-        <div className=" absolute top-0 left-0  max-w-7xl w-full z-20 relative">
-          <ThemeSwitch />
-        </div>
+          <div className=" absolute top-0 left-0  max-w-7xl w-full z-20 relative">
+            <ThemeSwitch />
+          </div>
+          <FloatingNav navItems={navItems} />
           <Hero />
         </div>
-      
       </main>
     </>
   );
